560ed351cfce40711d73b80dc0d0367fb868fc1e
Juan Vicente Vazquez G <juanvicentevazquezg@gmail.com>
2019-06-26 08:54:12 +0200

    Init

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-class Preproduct{
+class Preproduct {
   contructor(description, price, qty) {
     this.description = description;
     this.price = price;
@@ -6,10 +6,14 @@ class Preproduct{
   }
 }
 
-let product=new Preproduct;
-let store=[];
+let product = new Preproduct();
+let store = [];
 
-function deleteItem(event) {}
+function deleteItem(product) {
+  const productToRemove = product.target.parentNode.parentNode;
+  const AllProduct = document.getElementsByClassName("products")[0];
+  AllProduct.removeChild(productToRemove);
+}
 
 function getPriceByProduct(itemNode) {}
 
@@ -27,11 +31,16 @@ function createItemNode(dataType, itemData) {}
 
 function createNewItemRow(itemName, itemUnitPrice) {}
 
-function createNewItem() {}
+function createNewItem(product) {
+  
+}
 
 window.onload = function() {
-  
-  var calculatePriceButton = document.getElementById("calc-prices-button");
+  // let allProducts=document.getElementsByClassName("wrapper-product");
+  //  let id=document.getElementById("p1");
+  //  let products=document.getElementsByClassName("products")[0];
+  //  products.removeChild(id);
+  /*  var calculatePriceButton = document.getElementById("calc-prices-button");
   var createItemButton = document.getElementById("new-item-create");
   var deleteButtons = document.getElementsByClassName("btn-delete");
 
@@ -40,5 +49,16 @@ window.onload = function() {
 
   for (var i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].onclick = deleteItem;
-  }
+  } */
+  //Programming buttons
+  let product1 = document
+    .getElementsByClassName("wrapper-product")[0]
+    .querySelector(".btn-delete");
+
+  let product2 = document
+    .getElementsByClassName("wrapper-product")[1]
+    .querySelector(".btn-delete");
+
+  product1.addEventListener("click", deleteItem);
+  product2.addEventListener("click", deleteItem);
 };
