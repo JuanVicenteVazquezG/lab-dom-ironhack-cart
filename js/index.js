772bed2a7256c0c53ef150240a63f805c455ee4a
Juan Vicente Vazquez G <juanvicentevazquezg@gmail.com>
2019-06-26 15:28:43 +0200

    functions

--- js/index.js
+++ js/index.js
@@ -1,14 +1,3 @@
-class Preproduct {
-  contructor(description, price, qty) {
-    this.description = description;
-    this.price = price;
-    this.qty = qty;
-  }
-}
-
-let product = new Preproduct();
-let store = [];
-
 function deleteItem(product) {
   const productToRemove = product.target.parentNode.parentNode;
   const AllProduct = document.getElementsByClassName("products")[0];
@@ -17,13 +6,39 @@ function deleteItem(product) {
 
 function getPriceByProduct(itemNode) {}
 
-function updatePriceByProduct(productPrice, index) {}
+function updatePriceByProduct(inputEventKeypressed) {
+  inputEventKeypressed.target.value;
+}
+//aqui
+function getTotalPrice() {
+  let quantity = 0;
+  let productPrice = 0;
+  let products = document.getElementsByClassName("wrapper-product");
+  let pricePerUnity = 0;
+  for (let i = 0; i < products.length; i++) {
+    //quantity = products[i]
+    quantity = parseInt(products[i].childNodes[4].childNodes[2].value.toString();
+    productPrice = parseFloat(
+      products[i].childNodes[2].childNodes[0].innerHTML.slice(1).toString()
+    );
+     pricePerUnity=product[i].
+    (quantity * productPrice)
+  }
+  return finalPrice;
+}
 
-function getTotalPrice() {}
+function showPrice() {
+  let h2FinalPrice = document.getElementById("finalprice");
+  h2FinalPrice.innerHTML = "$" + getTotalPrice().toString();
+  let thePrice = document.getElementsByClassName("theprice")[0];
+  thePrice.style.display = "block;";
+}
 
 function createQuantityInput() {}
 
-function createDeleteButton() {}
+function createDeleteButton(deleteButtonObject) {
+  deleteButtonObject.addEventListener("click", deleteItem);
+}
 
 function createQuantityNode() {}
 
@@ -31,34 +46,46 @@ function createItemNode(dataType, itemData) {}
 
 function createNewItemRow(itemName, itemUnitPrice) {}
 
-function createNewItem(product) {
-  
+function createNewItem(description, price, qty) {
+  let divProduct = document.getElementsByClassName("products")[0];
+  let divWrapper = document.createElement("div");
+  divWrapper.setAttribute("class", "wrapper-product");
+
+  let nameProduct = description;
+  let priceProduct = "$" + price.toString();
+
+  divWrapper.innerHTML = `<div class="productname"><span>${nameProduct}</span></div>
+   <div class="price"><span>${priceProduct}</span></div>
+   <div class="qty">
+   <label >QTY</label><input id="qty" type="text" value="${qty}" placeholder="Introduce the units..." />
+   </div>
+  <div class="totalpriceproduct" ><span>$0</span></div>
+  <div class="delete-button">
+  <button class="btn-delete">Delete</button>
+  </div>`;
+
+  divProduct.appendChild(divWrapper);
+
+  //Programing button
+  let deleteButton = divWrapper.getElementsByClassName("btn-delete")[0];
+  createDeleteButton(deleteButton);
+
+  //Programming input of qty
+  let qtyInput = document.getElementById("qty");
+  qtyInput.addEventListener("keyup", updatePriceByProduct);
 }
 
 window.onload = function() {
-  // let allProducts=document.getElementsByClassName("wrapper-product");
-  //  let id=document.getElementById("p1");
-  //  let products=document.getElementsByClassName("products")[0];
-  //  products.removeChild(id);
-  /*  var calculatePriceButton = document.getElementById("calc-prices-button");
-  var createItemButton = document.getElementById("new-item-create");
-  var deleteButtons = document.getElementsByClassName("btn-delete");
+  let body = document.getElementsByTagName("body")[0];
+  let divProducts = document.createElement("div");
+  divProducts.setAttribute("class", "products");
+  body.appendChild(divProducts);
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  //Creating elements by default
+  createNewItem("IronBubble-head", 25.0, 0);
+  createNewItem("IronShirt", 15.0, 0);
 
-  for (var i = 0; i < deleteButtons.length; i++) {
-    deleteButtons[i].onclick = deleteItem;
-  } */
-  //Programming buttons
-  let product1 = document
-    .getElementsByClassName("wrapper-product")[0]
-    .querySelector(".btn-delete");
-
-  let product2 = document
-    .getElementsByClassName("wrapper-product")[1]
-    .querySelector(".btn-delete");
-
-  product1.addEventListener("click", deleteItem);
-  product2.addEventListener("click", deleteItem);
+  //Programming button calculate Prices
+  let calculatePricesB = document.getElementsByClassName("btn-success")[0];
+  calculatePricesB.addEventListener("click", showPrice);
 };
